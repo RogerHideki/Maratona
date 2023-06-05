e6d2f13ef474c510488fd120d2c739d60db7fd08
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2023-06-05 17:27:48 -0400

    codigoAllanTorpedo

--- beecrowd/Exs19XX/Ex1910/beecrowdEx1910.cpp
+++ beecrowd/Exs19XX/Ex1910/beecrowdEx1910.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <climits>
-
-#define INF INT_MAX
+#include <bitset>
 
 using namespace std;
 
-void bfs(vector<vector<int>> &grafo, vector<int> &distancias, int u) {
+bitset<100001> canais;
+
+int bfs(int u, int d) {
     queue<int> q;
     q.push(u);
-    distancias[u] = 0;
+    int level = -1;
     while (!q.empty()) {
-        u = q.front();
-        q.pop();
-        int size = grafo[u].size();
-        for (int i = 0; i < size; i++) {
-            int v = grafo[u][i];
-            if (distancias[v] == INF) {
-                distancias[v] = distancias[u] + 1;
-                q.push(v);
-            }
+        level++;
+        int tamanho = q.size();
+        for (int i = 0; i < tamanho; i++) {
+            u = q.front();
+            q.pop();
+            if (u == d) return level;
+            if (u != 100000 && canais[u + 1]) q.push(u + 1), canais[u + 1] = 0;
+            if (u != 1 && canais[u - 1]) q.push(u - 1), canais[u - 1] = 0;
+            if (u <= 50000 && canais[2 * u]) q.push(2 * u), canais[2 * u] = 0;
+            if (u <= 33333 && canais[3 * u]) q.push(3 * u), canais[3 * u] = 0;
+            if ((u & 1) == 0 && canais[u / 2]) q.push(u / 2), canais[u / 2] = 0;
         }
     }
+    return -1;
 }
 
 int main() {
@@ -31,24 +33,12 @@ int main() {
     int o, d, k, deletado;
 
     while (cin >> o >> d >> k && !(o == 0 && d == 0 && k == 0)) {
-        vector<bool> canais(100001, true);
+        canais.set();
         for (int i = 0; i < k; i++) {
             cin >> deletado;
-            canais[deletado] = false;
+            canais[deletado] = 0;
         }
-        vector<vector<int>> grafo(100001 - k);
-        for (int i = 1; i <= 100000; i++) {
-            if (canais[i]) {
-                if (i != 100000 && canais[i + 1]) grafo[i].push_back(i + 1);
-                if (i != 1 && canais[i - 1]) grafo[i].push_back(i - 1);
-                if (i <= 50000 && canais[2 * i]) grafo[i].push_back(2 * i);
-                if (i <= 33333 && canais[3 * i]) grafo[i].push_back(3 * i);
-                if ((i & 1) == 0 && canais[i / 2]) grafo[i].push_back(i / 2);
-            }
-        }
-        vector<int> distancias(100001, INF);
-        bfs(grafo, distancias, o);
-        cout << (distancias[d] == INF ? -1 : distancias[d]) << "\n";
+        cout << bfs(o, d) << "\n";
     }
 
     return 0;

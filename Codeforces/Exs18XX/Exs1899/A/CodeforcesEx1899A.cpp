913d1983441f695bc3fa1c02509a5f41da8d1569
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2023-11-17 13:04:42 -0400

    1117

---
+++ Codeforces/Exs18XX/Exs1899/A/CodeforcesEx1899A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t, n;
+    cin >> t;
+    while(t--) {
+        cin >> n;
+        if (n % 3) cout << "First\n";
+        else cout << "Second\n";
+    }
+    return 0;
+}

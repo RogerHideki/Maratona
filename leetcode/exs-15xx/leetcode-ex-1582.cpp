c033856627be50bdffb01099ef4dae68b448e3cd
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2023-12-13 21:03:20 -0400

    1213

--- leetcode/exs-15xx/leetcode-ex-1582.cpp
+++ leetcode/exs-15xx/leetcode-ex-1582.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int numSpecial(vector <vector<int>> &mat) {
-        vector <pair<int, int>> uns;
+        vector <pair<int, int>> ones;
         int m = mat.size();
-        vector<int> pesoLinhas(m, 0);
+        vector<int> onesRow(m, 0);
         int n = mat[0].size();
-        vector<int> pesoColunas(n, 0);
+        vector<int> onesCol(n, 0);
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (mat[i][j]) {
-                    if (!pesoLinhas[i] && !pesoColunas[j]) uns.push_back({i, j});
-                    pesoLinhas[i]++;
-                    pesoColunas[j]++;
+                    if (!onesRow[i] && !onesCol[j]) ones.push_back({i, j});
+                    onesRow[i]++;
+                    onesCol[j]++;
                 }
             }
         }
         int ans = 0;
-        for (auto [i, j]: uns) {
-            if (pesoLinhas[i] == 1 && pesoColunas[j] == 1) ans++;
+        for (auto [i, j]: ones) {
+            if (onesRow[i] == 1 && onesCol[j] == 1) ans++;
         }
         return ans;
     }

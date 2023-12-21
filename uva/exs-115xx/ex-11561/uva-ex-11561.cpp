abdc7e8532c96b48fa5de283e29cb0588e1390f7
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2023-12-21 00:04:17 -0400

    1221

---
+++ uva/exs-115xx/ex-11561/uva-ex-11561.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<char>> grid;
+int ans;
+vector<int> dx = {-1, 0, 1, 0};
+vector<int> dy = {0, 1, 0, -1};
+
+void floodfill(int ux, int uy) {
+    if (grid[ux][uy] == 'G') ans++;
+    grid[ux][uy] = '#';
+    for (int i = 0; i < 4; i++) {
+        int vx = ux + dx[i];
+        int vy = uy + dy[i];
+        if (grid[vx][vy] == 'T') return;
+    }
+    for (int i = 0; i < 4; i++) {
+        int vx = ux + dx[i];
+        int vy = uy + dy[i];
+        if (grid[vx][vy] != '#') floodfill(vx, vy);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int w, h;
+    while (cin >> w >> h) {
+        grid.assign(h, vector<char>(w));
+        int ux, uy;
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                cin >> grid[i][j];
+                if (grid[i][j] != 'P') continue;
+                ux = i;
+                uy = j;
+            }
+        }
+        ans = 0;
+        floodfill(ux, uy);
+        cout << ans << '\n';
+    }
+    return 0;
+}

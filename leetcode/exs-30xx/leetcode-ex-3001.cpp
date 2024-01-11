db2a9d9b4f583be211b60f013c9a754bf8e77989
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2024-01-11 14:55:30 -0400

    0111

--- leetcode/exs-30xx/leetcode-ex-3001.cpp
+++ leetcode/exs-30xx/leetcode-ex-3001.cpp
@@ -4,21 +4,21 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         if (a == e) {
-            if (c == e) {
+            if (a == c) {
                 if ((b < d && d < f) || (f < d && d < b)) return 2;
                 return 1;
             }
             return 1;
         }
         if (b == f) {
-            if (d == f) {
+            if (b == d) {
                 if ((a < c && c < e) || (e < c && c < a)) return 2;
                 return 1;
             }
             return 1;
         }
         if (abs(c - e) == abs(d - f)) {
-            if (abs(a - e) == abs(b - f)) {
+            if (abs(c - a) == abs(d - b) && abs(a - e) == abs(b - f)) {
                 if ((c < a && a < e) || (e < a && a < c)) return 2;
                 return 1;
             }

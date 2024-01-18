af01c624a3e016ca55e50a99246e4bbbbc8d7ba7
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2024-01-18 11:07:31 -0400

    0118

--- leetcode/exs-00xx/leetcode-ex-0070.cpp
+++ leetcode/exs-00xx/leetcode-ex-0070.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int memo[46];
+    vector<int> memo;
     int n;
 
     int f(int i) {
         if (i == n) return 1;
         if (i > n) return 0;
-        if (memo[i] != -1) return memo[i];
+        if (memo[i]) return memo[i];
         return memo[i] = f(i + 1) + f(i + 2);
     }
 
     int climbStairs(int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         this->n = n;
-        memset(memo, -1, sizeof memo);
+        memo.assign(n, 0);
         return f(0);
     }
 };
--- leetcode/exs-12xx/leetcode-ex-1207.cpp
+++ leetcode/exs-12xx/leetcode-ex-1207.cpp
@@ -4,8 +4,8 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         unordered_map<int, int> m;
-        unordered_set<int> s;
         for (auto &num: arr) m[num]++;
+        unordered_set<int> s;
         for (auto &it: m) {
             if (s.find(it.second) != s.end()) return false;
             s.insert(it.second);

7df9bfed737b92f1c428bd5caa22adfb986e5334
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2024-02-05 10:44:08 -0400

    0205

--- leetcode/exs-00xx/leetcode-ex-0076.cpp
+++ leetcode/exs-00xx/leetcode-ex-0076.cpp
@@ -5,30 +5,35 @@ public:
         cin.tie(NULL);
         vector<int> v(52, 0);
         for (auto &c: t) (c <= 'Z') ? v[c - 'A']-- : v[c - 'a' + 26]--;
-        int n = s.size();
         int i = 0;
-        int idxI = -1;
-        int idxJ = n;
-        for (int j = 0; j < n; j++) {
+        int j = 0;
+        int n = s.size();
+        while (j != n) {
             (s[j] <= 'Z') ? v[s[j] - 'A']++ : v[s[j] - 'a' + 26]++;
-            bool isAns = true;
-            for (auto &amount: v) {
-                if (amount >= 0) continue;
-                isAns = false;
-                break;
-            }
-            while (isAns) {
-                int idx = ((s[i] <= 'Z') ? s[i] - 'A' : s[i] - 'a' + 26);
-                v[idx]--;
-                i++;
-                if (v[idx] >= 0) continue;
-                if ((idxJ - idxI) <= (j - (i - 1))) break;
-                idxI = i - 1;
+            while ((i != 52) && (v[i] >= 0)) i++;
+            if (i == 52) break;
+            j++;
+        }
+        if (i != 52) return "";
+        i = 0;
+        int idxI = 1;
+        int idxJ = n;
+        while (j != n) {
+            int hash = ((s[i] <= 'Z') ? s[i] - 'A' : s[i] - 'a' + 26);
+            v[hash]--;
+            i++;
+            if (v[hash] >= 0) continue;
+            j++;
+            if ((idxJ - idxI) > (j - i)) {
+                idxI = i;
                 idxJ = j;
-                break;
+            }
+            while (j != n) {
+                (s[j] <= 'Z') ? v[s[j] - 'A']++ : v[s[j] - 'a' + 26]++;
+                if (v[hash] == 0) break;
+                j++;
             }
         }
-        if ((idxI == -1) && (idxJ == n)) return "";
-        return s.substr(idxI, idxJ - idxI + 1);
+        return s.substr(idxI - 1, idxJ - idxI + 1);
     }
 };

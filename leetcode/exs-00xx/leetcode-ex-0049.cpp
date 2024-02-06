1a2423da2ba2b26d8e07589ad135068610eef335
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2024-02-06 09:42:35 -0400

    0206

--- leetcode/exs-00xx/leetcode-ex-0049.cpp
+++ leetcode/exs-00xx/leetcode-ex-0049.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    vector <vector<string>> groupAnagrams(vector <string> &strs) {
-        vector <vector<string>> output;
-        unordered_map <string, vector<string>> m;
-        int size = strs.size();
-        for (int i = 0; i < size; i++) {
-            string s = strs[i];
-            sort(strs[i].begin(), strs[i].end());
-            m[strs[i]].push_back(s);
+    vector<vector<string>> groupAnagrams(vector<string> &strs) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        map<string, vector<string>> m;
+        for (auto &str: strs) {
+            string s = str;
+            sort(str.begin(), str.end());
+            m[str].emplace_back(s);
         }
-        for (auto it = m.begin(); it != m.end(); it++) output.push_back(it->second);
+        vector<vector<string>> output(m.size());
+        int i = 0;
+        for (auto &it: m) {
+            output[i] = it.second;
+            i++;
+        }
         return output;
     }
 };

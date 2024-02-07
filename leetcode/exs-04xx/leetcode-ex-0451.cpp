75f3e26fe0bc9e3ebff9066572217c80672b3bf9
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2024-02-07 07:51:29 -0400

    0207

--- leetcode/exs-04xx/leetcode-ex-0451.cpp
+++ leetcode/exs-04xx/leetcode-ex-0451.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
+#define pic pair<int, char>
+
     string frequencySort(string s) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         map<char, int> m1;
         for (auto &c: s) m1[c]++;
-        map<int, string> m2;
-        for (auto &[key, value]: m1) m2[value] += key;
+        priority_queue<pic, vector<pic>> pq;
+        for (auto &[key, value]: m1) pq.emplace(value, key);
         string ans = "";
-        for (auto it = m2.rbegin(); it != m2.rend(); it++) {
-            for (auto &c: it->second) {
-                for (int i = 0; i < it->first; i++) ans += c;
-            }
+        while (!pq.empty()) {
+            auto [first, second] = pq.top();
+            pq.pop();
+            for (int i = 0; i < first; i++) ans += second;
         }
         return ans;
     }

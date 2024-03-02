d7752da49adb8815de87ecf44dd66ee0d1f6daa9
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2024-03-01 20:33:49 -0400

    0301

--- leetcode/exs-09xx/leetcode-ex-0977.cpp
+++ leetcode/exs-09xx/leetcode-ex-0977.cpp
@@ -3,8 +3,21 @@ public:
     vector<int> sortedSquares(vector<int> &nums) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        for (int i = nums.size() - 1; i >= 0; i--) nums[i] *= nums[i];
-        sort(nums.begin(), nums.end());
-        return nums;
+        int n = nums.size();
+        vector<int> ans(n);
+        int left = 0;
+        int right = n - 1;
+        int i = right;
+        while (left <= right) {
+            if (abs(nums[left]) > abs(nums[right])) {
+                ans[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                ans[i] = nums[right] * nums[right];
+                right--;
+            }
+            i--;
+        }
+        return ans;
     }
 };

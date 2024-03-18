2dd81224f168e646597abb1ba27122a5f3b6f44e
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2024-03-18 18:42:53 -0400

    0318

---
+++ leetcode/exs-04xx/leetcode-ex-0452.cpp
@@ -0,0 +1,23 @@
+int xlr8 = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie();
+    return 0;
+}();
+
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>> &points) {
+        int n = points.size();
+        sort(points.begin(), points.end());
+        int right = points[0][1];
+        int ans = 1;
+        for (int i = 1; i < n; i++) {
+            if (right >= points[i][0]) right = min(right, points[i][1]);
+            else {
+                ans++;
+                right = points[i][1];
+            }
+        }
+        return ans;
+    }
+};

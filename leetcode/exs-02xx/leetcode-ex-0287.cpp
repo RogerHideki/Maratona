86287ddb9738d168d8b506229594d4665cc53de5
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2024-03-24 17:50:30 -0400

    0324

---
+++ leetcode/exs-02xx/leetcode-ex-0287.cpp
@@ -0,0 +1,23 @@
+int xrl8 = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    int findDuplicate(vector<int> &nums) {
+        int slow = nums[0];
+        int fast = slow;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
